6d1f09510439c523d4116dff92cd471e6481ddaf
omer-X-kiyak <89635390+omer-X-kiyak@users.noreply.github.com>
2023-04-06 21:14:01 +0300

    Add files via upload

---
+++ soru-bot.py
@@ -0,0 +1,27 @@
+import json
+
+# Soru JSON verisini yükle
+with open('soru.json', 'r', encoding='utf-8') as f:
+    soru_json = json.load(f)
+
+while True:
+    # Her soru ve seçenekleri ekrana yazdır
+    for soru in soru_json:
+        print(soru['soru'])
+        for cevap in soru['cevaplar']:
+            print(cevap)
+
+        # Kullanıcının cevabını al
+        kullanici_cevabi = input("Cevabınızı girin (A, B, C veya D) veya çıkmak için 'q' basın: ").upper()
+
+        if kullanici_cevabi == 'Q':
+            print("Programdan çıkılıyor...")
+            exit()
+
+        # Doğru cevabı kontrol et
+        if kullanici_cevabi == soru['dogruCevap']:
+            print("Tebrikler, doğru cevap!")
+        else:
+            print("Maalesef, yanlış cevap. Lütfen cevabınızı gözden geçirmenizi öneririm. Çünkü, Doğru cevap: " + soru['dogruCevap'])+" seçenegdir"
+
+print("Programdan çıkıldı.")
